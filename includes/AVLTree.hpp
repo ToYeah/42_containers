61299f23fda27563ff7b3617b0c0ec4f9ef8e183
Taisei Toya <tytaisei0102@gmail.com>
2022-04-30 22:07:36 +0200

    AVLNodeをインナークラスに変更

--- includes/AVLTree.hpp
+++ includes/AVLTree.hpp
@@ -11,201 +11,201 @@
 namespace ft {
 
 template <typename T>
-struct AVLNode {
-  T data;
-  AVLNode* left;
-  AVLNode* right;
-  AVLNode* parent;
-  int height;
-  int bias;
-  int size;
-
-  AVLNode(const T& value = T(), AVLNode* parent = NULL)
-      : data(value),
-        left(NULL),
-        right(NULL),
-        parent(parent),
-        height(1),
-        size(1),
-        bias(0) {}
-
-  AVLNode(const AVLNode& src) { *this = src; }
-
-  ~AVLNode() {
-    if (parent) {
-      if (isRightChild()) {
-        parent->right = NULL;
-      } else {
-        parent->left = NULL;
-      }
-    }
-  }
-
-  AVLNode& operator=(const AVLNode& rhs) {
-    if (this != &rhs) {
-      data = rhs.data;
-      left = rhs.left;
-      right = rhs.right;
-      parent = rhs.parent;
-      height = rhs.height;
-      bias = rhs.bias;
-      size = rhs.size;
-    }
-    return *this;
-  }
-
-  void printTreeGraph() {
-    if (left) left->printTreeGraph();
-    std::cout << data << " [label=\"" << data << "\nbias: " << bias
-              << "\nheight: " << height << "\nsize: " << size << "\"]"
-              << std::endl;
-    if (left) {
-      std::cout << data << "->" << left->data << " [color = blue];"
-                << std::endl;
-    }
-    if (right) {
-      std::cout << data << "->" << right->data << " [color = red];"
-                << std::endl;
-    }
-    if (right) right->printTreeGraph();
-  }
-
-  bool compare(const T& value) {
-    if (data < value) return true;
-    return false;
-  }
-
-  bool equal(const T& value) {
-    return !(compare(value)) && !(AVLNode(value).compare(data));
-  }
-
-  void updateNodeInfo() {
-    int right_h = right ? right->height : 0;
-    int left_h = left ? left->height : 0;
-    height = 1;
-    height += left_h > right_h ? left_h : right_h;
-    bias = left_h - right_h;
-
-    int right_s = right ? right->size : 0;
-    int left_s = left ? left->size : 0;
-    size = 1;
-    size += right_s + left_s;
-  }
-
-  void joinNode(AVLNode* parent, bool is_right_child, AVLNode* child) {
-    if (is_right_child) {
-      parent->right = child;
-    } else {
-      parent->left = child;
-    }
-    if (child) {
-      child->parent = parent;
-    }
-  }
-
-  void rotate() {
-    if (bias > 1) {
-      if (left && left->bias <= -1) {
-        left->rotateL();
-        rotateR();
-      } else {
-        rotateR();
-      }
-
-    } else if (bias < -1) {
-      if (right && right->bias >= 1) {
-        right->rotateR();
-        rotateL();
-      } else {
-        rotateL();
-      }
-    }
-  }
-
-  void rotateL() {
-    AVLNode* pivot = right;
-    AVLNode* old_parent = parent;
-    bool is_right_child = isRightChild();
-
-    joinNode(this, RIGHT, pivot->left);
-    joinNode(pivot, LEFT, this);
-    joinNode(old_parent, is_right_child, pivot);
-    this->updateNodeInfo();
-    pivot->updateNodeInfo();
-    old_parent->updateNodeInfo();
-  }
-
-  void rotateR() {
-    AVLNode* pivot = left;
-    AVLNode* old_parent = parent;
-    bool is_right_child = isRightChild();
-
-    joinNode(this, LEFT, pivot->right);
-    joinNode(pivot, RIGHT, this);
-    joinNode(old_parent, is_right_child, pivot);
-    this->updateNodeInfo();
-    pivot->updateNodeInfo();
-    old_parent->updateNodeInfo();
-  }
-
-  void updateNode() {
-    updateNodeInfo();
-    rotate();
-  }
-
-  AVLNode** getNextDirection(const T& value) {
-    return compare(value) ? &right : &left;
-  }
-
-  AVLNode* getMaxNode() {
-    AVLNode* featured = this;
-    while (featured->right) {
-      featured = featured->right;
-    }
-    return featured;
-  }
-
-  AVLNode* getMinNode() {
-    AVLNode* featured = this;
-    while (featured->left) {
-      featured = featured->left;
-    }
-    return featured;
-  }
-
-  AVLNode* getNextNode() {
-    if (right) {
-      return right->getMinNode();
-    } else if (!isRightChild()) {
-      return parent;
-    }
-    AVLNode* featured = parent;
-    while (featured->isRightChild()) {
-      featured = featured->parent;
-    }
-    return featured->parent;
-  }
-
-  AVLNode* getPrevNode() {
-    if (left) {
-      return left->getMaxNode();
-    } else if (isRightChild()) {
-      return parent;
-    }
-    AVLNode* featured = parent;
-    while (featured->parent->parent && !(featured->isRightChild())) {
-      featured = featured->parent;
-    }
-    return featured->parent;
-  }
-
-  bool isRightChild() { return this->parent->right == this ? true : false; }
-};
-
-template <typename T>
 class AVLTree {
  private:
-  typedef AVLNode<T> Node;
+  struct AVLNode {
+    T data;
+    AVLNode* left;
+    AVLNode* right;
+    AVLNode* parent;
+    int height;
+    int bias;
+    int size;
+
+    AVLNode(const T& value = T(), AVLNode* parent = NULL)
+        : data(value),
+          left(NULL),
+          right(NULL),
+          parent(parent),
+          height(1),
+          size(1),
+          bias(0) {}
+
+    AVLNode(const AVLNode& src) { *this = src; }
+
+    ~AVLNode() {
+      if (parent) {
+        if (isRightChild()) {
+          parent->right = NULL;
+        } else {
+          parent->left = NULL;
+        }
+      }
+    }
+
+    AVLNode& operator=(const AVLNode& rhs) {
+      if (this != &rhs) {
+        data = rhs.data;
+        left = rhs.left;
+        right = rhs.right;
+        parent = rhs.parent;
+        height = rhs.height;
+        bias = rhs.bias;
+        size = rhs.size;
+      }
+      return *this;
+    }
+
+    void printTreeGraph() {
+      if (left) left->printTreeGraph();
+      std::cout << data << " [label=\"" << data << "\nbias: " << bias
+                << "\nheight: " << height << "\nsize: " << size << "\"]"
+                << std::endl;
+      if (left) {
+        std::cout << data << "->" << left->data << " [color = blue];"
+                  << std::endl;
+      }
+      if (right) {
+        std::cout << data << "->" << right->data << " [color = red];"
+                  << std::endl;
+      }
+      if (right) right->printTreeGraph();
+    }
+
+    bool compare(const T& value) {
+      if (data < value) return true;
+      return false;
+    }
+
+    bool equal(const T& value) {
+      return !(compare(value)) && !(AVLNode(value).compare(data));
+    }
+
+    void updateNodeInfo() {
+      int right_h = right ? right->height : 0;
+      int left_h = left ? left->height : 0;
+      height = 1;
+      height += left_h > right_h ? left_h : right_h;
+      bias = left_h - right_h;
+
+      int right_s = right ? right->size : 0;
+      int left_s = left ? left->size : 0;
+      size = 1;
+      size += right_s + left_s;
+    }
+
+    void joinNode(AVLNode* parent, bool is_right_child, AVLNode* child) {
+      if (is_right_child) {
+        parent->right = child;
+      } else {
+        parent->left = child;
+      }
+      if (child) {
+        child->parent = parent;
+      }
+    }
+
+    void rotate() {
+      if (bias > 1) {
+        if (left && left->bias <= -1) {
+          left->rotateL();
+          rotateR();
+        } else {
+          rotateR();
+        }
+
+      } else if (bias < -1) {
+        if (right && right->bias >= 1) {
+          right->rotateR();
+          rotateL();
+        } else {
+          rotateL();
+        }
+      }
+    }
+
+    void rotateL() {
+      AVLNode* pivot = right;
+      AVLNode* old_parent = parent;
+      bool is_right_child = isRightChild();
+
+      joinNode(this, RIGHT, pivot->left);
+      joinNode(pivot, LEFT, this);
+      joinNode(old_parent, is_right_child, pivot);
+      this->updateNodeInfo();
+      pivot->updateNodeInfo();
+      old_parent->updateNodeInfo();
+    }
+
+    void rotateR() {
+      AVLNode* pivot = left;
+      AVLNode* old_parent = parent;
+      bool is_right_child = isRightChild();
+
+      joinNode(this, LEFT, pivot->right);
+      joinNode(pivot, RIGHT, this);
+      joinNode(old_parent, is_right_child, pivot);
+      this->updateNodeInfo();
+      pivot->updateNodeInfo();
+      old_parent->updateNodeInfo();
+    }
+
+    void updateNode() {
+      updateNodeInfo();
+      rotate();
+    }
+
+    AVLNode** getNextDirection(const T& value) {
+      return compare(value) ? &right : &left;
+    }
+
+    AVLNode* getMaxNode() {
+      AVLNode* featured = this;
+      while (featured->right) {
+        featured = featured->right;
+      }
+      return featured;
+    }
+
+    AVLNode* getMinNode() {
+      AVLNode* featured = this;
+      while (featured->left) {
+        featured = featured->left;
+      }
+      return featured;
+    }
+
+    AVLNode* getNextNode() {
+      if (right) {
+        return right->getMinNode();
+      } else if (!isRightChild()) {
+        return parent;
+      }
+      AVLNode* featured = parent;
+      while (featured->isRightChild()) {
+        featured = featured->parent;
+      }
+      return featured->parent;
+    }
+
+    AVLNode* getPrevNode() {
+      if (left) {
+        return left->getMaxNode();
+      } else if (isRightChild()) {
+        return parent;
+      }
+      AVLNode* featured = parent;
+      while (featured->parent->parent && !(featured->isRightChild())) {
+        featured = featured->parent;
+      }
+      return featured->parent;
+    }
+
+    bool isRightChild() { return this->parent->right == this ? true : false; }
+  };
+
+ private:
+  typedef AVLNode Node;
   Node*& root;
   Node end;
 
