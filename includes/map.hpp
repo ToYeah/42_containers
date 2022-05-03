d5acd5a06fc0ccdc3c4fa352fff0d069a6e13bed
Taisei Toya <tytaisei0102@gmail.com>
2022-05-03 10:51:00 +0200

    value_compareを作成

--- includes/map.hpp
+++ includes/map.hpp
@@ -1,16 +1,18 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include <functional>
+
 #include "AVLTree.hpp"
 #include "reverse_iterator.hpp"
 
 namespace ft {
 
 template <class Key, class T, class Compare = std::less<Key>,
-          class Allocator = std::allocator<pair<const Key, T> > >
+          class Allocator = std::allocator<pair<const Key, T>>>
 class map {
  private:
-  typedef AVLTree<Key, T, Compare, Allocator> tree;
+  typedef AVLTree<Key, T, Compare, Allocator> avl_tree;
 
  public:
   typedef Key key_type;
@@ -24,8 +26,35 @@ class map {
   typedef typename allocator_type::const_reference const_reference;
   typedef typename allocator_type::pointer pointer;
   typedef typename allocator_type::const_pointer const_pointer;
-  typedef typename tree::tree_iterator iterator;
+  typedef typename avl_tree::tree_iterator iterator;
   typedef reverse_iterator<iterator> reverse_iterator;
+
+  class value_compare
+      : public std::binary_function<value_type, value_type, bool> {
+   protected:
+    Compare comp;
+    value_compare(Compare c) : comp(c) {}
+
+   public:
+    typedef bool result_type;
+    bool operator()(const value_type& x, const value_type& y) const {
+      return comp(x.first, y.first);
+    }
+  };
+
+ public:
+  avl_tree tree;
+
+ public:
+  explicit map(const key_compare& comp = key_compare(),
+               const allocator_type& alloc = allocator_type()) {}
+
+  template <class InputIterator>
+  map(InputIterator first, InputIterator last,
+      const key_compare& comp = key_compare(),
+      const allocator_type& alloc = allocator_type());
+
+  map(const map& x);
 };
 
 }  // namespace ft
