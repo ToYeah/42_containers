9d654117e5d6426e38f50c2c6fc94d950a57e3b0
Taisei Toya <tytaisei0102@gmail.com>
2022-05-10 17:55:20 +0200

    testの実行分けを追加

--- measurement/measurement.sh
+++ measurement/measurement.sh
@@ -6,18 +6,31 @@ function grepTest() {
 	cat ./measure_$1.cpp | grep '//' | sed -e 's/.* TEST: //g'
 }
 
-function measure() {
-rm $1_std.compare
-rm $1_ft.compare
+function clean() {
+	rm $1_std.compare
+	rm $1_ft.compare
+	rm $1_std.log
+	rm $1_ft.log
+	rm $1_test.log
+}
 
+function measure() {
+
+clean $1
 $cmpl measure_$1.cpp -D STD -o $1_std.compare 
 $cmpl measure_$1.cpp -D FT  -o $1_ft.compare 
 
 ./$1_std.compare > $1_std.log
 ./$1_ft.compare  > $1_ft.log
-grepTest $1 > $1_test_.log
+grepTest $1 > $1_test.log
 
-python3 measurement.py vector vector_std.log vector_ft.log vector_test_.log
+python3 measurement.py $1 $1_std.log $1_ft.log $1_test.log
 }
 
+if [ $# -eq 1 ];then
+	measure $1
+    exit 1
+fi
+
 measure vector
+measure stack
