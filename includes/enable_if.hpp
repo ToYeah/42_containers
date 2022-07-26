310912a25f2aa674d8d021d963cf886a8e5f9861
Taisei Toya <tytaisei0102@gmail.com>
2022-07-26 22:17:54 +0200

    不要な関数を削除

--- includes/enable_if.hpp
+++ includes/enable_if.hpp
@@ -8,7 +8,6 @@ struct integral_constant {
   static const T value = v;
   typedef T value_type;
   typedef integral_constant<T, v> type;
-  const operator T() { return v; }
 };
 
 typedef integral_constant<bool, true> true_type;
